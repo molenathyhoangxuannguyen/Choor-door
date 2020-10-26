3b4b2e2f75afd81fa72ee2766b715d5715fba483
Molena (Thy) Hoang-Xuan Nguyen <molenathyhoangxuannguyen@users.noreply.github.com>
2020-10-26 15:40:01 -0400

    update project

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const doorImage2 = document.getElementById('door2');
 const doorImage3 = document.getElementById('door3');
 const doorImage3 = document.getElementById('door4');
 const doorImage3 = document.getElementById('door5');
-const botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
+const botDoorPath = 'https://github.com/molenathyhoangxuannguyen/Chore-Door-Game/blob/master/img/Bomb.png';
 const beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
 const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
 
